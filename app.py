5df470e427151da80a18ac928bd51b0df2a62c1a
Chris H <crhowell1@gmail.com>
2016-08-16 15:52:47 -0700

    Add user registrations, login, and logout

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import (Flask, g, render_template, flash, redirect,
 from flask_bcrypt import check_password_hash
 from flask_login import (LoginManager, login_user, current_user,
                          login_required, logout_user)
-
+import forms
 import models
 
 DEBUG = True
@@ -38,6 +38,42 @@ def after_request(response):
 def index():
     return render_template('index.html')
 
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        models.User.create_user(
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(
+                models.User.email == form.email.data
+            )
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You're now logged in!")
+                return redirect(url_for('index'))
+            else:
+                flash("No user with that email/password combo")
+        except models.DoesNotExist:
+            flash("No user with that email/password combo")
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     models.initialize()
--- models.py
+++ models.py
@@ -8,24 +8,21 @@ DATABASE = SqliteDatabase('journal.db')
 
 
 class User(UserMixin, Model):
-    username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=120)
     created_at = DateTimeField(default=datetime.datetime.now)
     last_modified = DateTimeField(default=datetime.datetime.now)
-    is_admin = BooleanField(default=False)
 
     class Meta:
         database = DATABASE
 
     @classmethod
-    def create_user(cls, username, email, password, admin=False):
+    def create_user(cls, email, password):
         try:
             cls.create(
-                username=username,
                 email=email,
-                password=generate_password_hash(password),
-                is_admin=admin)
+                password=generate_password_hash(password)
+            )
         except IntegrityError:
             raise ValueError("User already exists")
 
